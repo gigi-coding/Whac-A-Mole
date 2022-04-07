a3935c5090213de914c384488830a40770dfd628
gigi-coding <101548840+gigi-coding@users.noreply.github.com>
2022-04-07 11:50:22 -0600

    working on pseudocode

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,6 +1,22 @@
 // Define all elements 
 const scoreBoard = document.querySelector(".score")
+const boardSquares = document.querySelector(".squares")
 const gameBoard = document.querySelector(".board")
 const timer = document.querySelector(".timer")
 const mole = 
 // Trying to figure out where to place the mole in HTML..?
+
+// Create a function that will randomize the mole on the gameboard
+
+function randomSquare(){}
+// Create a function that will allow the mole to be on said square for X amount of time using setInterval
+
+// Create a function to keep track of the time
+function timerUpdate 
+
+// Create a function for the scoreboard
+function scoreUpdate
+// Create a function that will have an eventListner on the gameboard squares so when it is clicked it will add a point to the scoreboard if there was a mole on it. 
+
+// 
+
