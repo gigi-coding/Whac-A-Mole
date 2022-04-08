b836d13508fad392f8a0ff2dfbf8b6a31c4b6e87
gigi-coding <101548840+gigi-coding@users.noreply.github.com>
2022-04-08 11:42:44 -0600

    FINALLY for my randomHole function to work! Yippeegit add .

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,22 +1,40 @@
 // Define all elements 
-const scoreBoard = document.querySelector(".score")
-const boardSquares = document.querySelector(".squares")
+const holes = document.querySelectorAll(".hole")
+const mole = document.querySelector(".mole")
 const gameBoard = document.querySelector(".board")
+const scoreBoard = document.querySelector(".score")
 const timer = document.querySelector(".timer")
-const mole = 
-// Trying to figure out where to place the mole in HTML..?
 
 // Create a function that will randomize the mole on the gameboard
 
-function randomSquare(){}
+
+function randomHole(){
+    holes.forEach(hole => {
+                                        // for each hole in our holes array
+        hole.classList.remove("mole")
+                                         // I want to get the hole and remove the mole if it exist in any of the holes 
+    })
+   
+    let randomMolePosition = holes[Math.floor(Math.random()*holes.length)]
+    console.log(randomMolePosition)
+}
+
+randomHole()
+
+
+
+
+
+
+
+
 // Create a function that will allow the mole to be on said square for X amount of time using setInterval
 
 // Create a function to keep track of the time
-function timerUpdate 
+// function timerUpdate 
 
 // Create a function for the scoreboard
-function scoreUpdate
-// Create a function that will have an eventListner on the gameboard squares so when it is clicked it will add a point to the scoreboard if there was a mole on it. 
+// function scoreUpdate
+// Create a function that will have an eventListner on the gameboard hole so when it is clicked it will add a point to the scoreboard if there was a mole on it. 
 
-// 
-
+//Create a function to stop the game when the timer ends
