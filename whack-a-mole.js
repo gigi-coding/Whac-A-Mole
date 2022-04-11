94dbf808e753b2298de98826703646a01aba3846
gigi-coding <101548840+gigi-coding@users.noreply.github.com>
2022-04-11 12:13:12 -0600

    eventListener with click event working & scoreboard updating on click

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,37 +1,24 @@
 // Define all elements 
 const holes = document.querySelectorAll(".hole")
-const mole = document.querySelector(".mole")
-const gameBoard = document.querySelector(".board")
+const mole = document.querySelectorAll(".mole")
 const scoreBoard = document.querySelector(".score")
 const timer = document.querySelector(".timer")
-
+let currentScore = 0;
 // Create a function that will randomize the mole on the gameboard
 
 
-function randomHole(){
-    holes.forEach(hole => {
-                                        // for each hole in our holes array
-        hole.classList.remove("mole")
-                                         // I want to get the hole and remove the mole if it exist in any of the holes 
-    })
-}
 let randomMolePosition =()=>{
  let randomMole = holes[Math.floor(Math.random()*holes.length)]
     randomMole.classList.add("mole")
     setTimeout(()=>{
         randomMole.classList.remove("mole")
-    },500)
+    },2000)
 }
 // randomMole()
-randomHole()
-setInterval(randomMolePosition, 500)
+// randomHole()
+setInterval(randomMolePosition, 1000)
 // Create a function that will allow the mole to be on said square for X amount of time using setInterval
-// function surpriseMole(){
-    
-// } 
 
-
-// setInterval(function name, time in miliseconds we want the function to run)
 
 
 // Create a function to keep track of the time
@@ -40,5 +27,28 @@ setInterval(randomMolePosition, 500)
 // Create a function for the scoreboard
 // function scoreUpdate
 // Create a function that will have an eventListner on the gameboard hole so when it is clicked it will add a point to the scoreboard if there was a mole on it. 
+holes.forEach(moles => {
+    moles.addEventListener("click", moleHit)
+            
+   
+})
 
-//Create a function to stop the game when the timer ends
+function moleHit() {
+      
+    console.log(event)
+    if (event.target.classList[1] === "mole"){
+      
+        currentScore ++;
+        console.log(currentScore);
+        scoreBoard.innerHTML = `Score ${currentScore}`
+    }
+    
+    console.log(scoreBoard.innerHTML)
+    return currentScore
+
+}
+
+    
+
+
+ //Create a function to stop the game when the timer ends
